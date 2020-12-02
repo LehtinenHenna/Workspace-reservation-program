8a9d62a0f7d48fdfa4d12489cabe334739ba62a6
AtteMK <atte09061995@gmail.com>
2020-12-02 22:17:57 +0200

    First Commit

--- resources/workspace.py
+++ resources/workspace.py
@@ -7,6 +7,7 @@ from models.workspace import Workspace
 from schemas.workspace import WorkspaceSchema
 
 workspace_schema = WorkspaceSchema()
+workspace_list_schema = WorkspaceSchema(many=True)
 
 
 class WorkspaceListResource(Resource):
@@ -30,3 +31,42 @@ class WorkspaceListResource(Resource):
         workspace.save()
 
         return workspace_schema.dump(workspace).data, HTTPStatus.CREATED
+
+    """Get all workspaces"""
+
+    def get(self):
+        """GET <- /workspeces"""
+        workspaces = Workspace.get_all()
+        return workspace_list_schema.dump(workspaces).data, HTTPStatus.OK
+
+
+class WorkspaceResource(Resource):
+    """Get specific workspace"""
+    
+    def get(self, name):
+        """GET <- /workspeces/name"""
+        workspace = Workspace.get_by_name(name=name)
+        if workspace is None:
+            return {'message':'Workspace not found'}, HTTPStatus.NOT_FOUND
+        return workspace_schema.dump(workspace).data, HTTPStatus.OK
+
+    def put(self, workspace_id):
+        """PUT -> /workspaces/workspace_id"""
+        json_data = request.get_json()
+        workspace = Workspace.get_by_id(workspace_id=workspace_id)
+        if workspace is None:
+            return {'message''Workspace not found':}, HTTPStatus.NOT_FOUND
+        workspace.name = json_data('name')
+        workspace.user_limit = json_data('user_limit')
+        workspace.available_from = json_data('available_from')
+        workspace.available_till = json_data('available_till')
+        workspace.save()
+        return workspace.data(), HTTPStatus.OK
+    
+    def delete(self, workspace_id):
+        """DELETE /workspaces/workspace_id"""
+        workspace = Workspace.get_by_id(workspace_id=workspace_id)
+        if workspace is None:
+            return {'message''Workspace not found':}, HTTPStatus.NOT_FOUND
+        workspace.delete()
+        return {}, HTTPStatus.NO_CONTENT
