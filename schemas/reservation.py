e77e74113a13bb82da8366edf5c152ed36974df8
LehtinenHenna <henna.lehtinen91@gmail.com>
2020-12-09 15:59:13 +0200

    reservation post works but needs time validation

--- schemas/reservation.py
+++ schemas/reservation.py
@@ -1,8 +1,9 @@
 from marshmallow import Schema, fields
+from schemas.user import UserSchema
 
 class ReservationSchema(Schema):
     id = fields.Integer(dump_only=True)
     start_time = fields.DateTime(required=True)
     end_time = fields.DateTime(required=True)
     workspace_id = fields.Integer()
-    user_id = fields.Integer()
+    username = fields.String(dump_only=True)
