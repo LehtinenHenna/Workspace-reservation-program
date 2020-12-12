10d5269559da2395f859b242b7b8c1b9b6d43813
AtteMK <atte09061995@gmail.com>
2020-12-12 13:32:23 +0200

    Commit 4

--- schemas/workspace.py
+++ schemas/workspace.py
@@ -1,11 +1,12 @@
 from marshmallow import Schema, fields, post_dump, validate, validates, ValidationError
+from datetime import datetime
 
 class WorkspaceSchema(Schema):
     id = fields.Integer(dump_only=True)
-    name = fields.String(required=True)
-    user_limit = fields.Integer()
-    available_from = fields.Time(16)
-    available_till = fields.Time(21)
+    name = fields.String(required=True, validate=[validate.Length(max=100)])
+    user_limit = fields.Integer(required=True)
+    available_from = fields.Time(required=True)
+    available_till = fields.Time(required=True)
 
     @post_dump(pass_many=True)
     def wrap(self, data, many, **kwargs):
@@ -18,16 +19,3 @@ class WorkspaceSchema(Schema):
         if value < 1:
             raise ValidationError('User limit can not be less than 1.')
 
-    @validates('available_from')
-    def validate_available_from(self, value):
-        if value < 16:
-            raise ValidationError('Available from must be greater than or equal to 16.')
-        if value > 20:
-            raise ValidationError('Available from must be less than 21.')
-
-    @validates('available_till')
-    def validate_available_till(self, value):
-        if value > 21:
-            raise ValidationError('Available till must be less than or equal to 21.')
-        if value < 17:
-            raise ValidationError('Available till mus be greater than 16.')
